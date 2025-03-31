a79f137c2c5533eb4d23753c7942d1a3c9fad811
ShinySylvie <jordynmbrown@gmail.com>
2025-03-30 21:35:51 -0400

    added login page in views.py and urls.py

--- pcsarchive/myapp/views.py
+++ pcsarchive/myapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'homepage.html')
+
+def login_page(request):
+    return render(request, 'login.html')
--- pcsarchive/pcsarchive/urls.py
+++ pcsarchive/pcsarchive/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import home_page
+from myapp.views import home_page, login_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
+    path('login/', login_page, name='login'),  # Add this line
 ]
